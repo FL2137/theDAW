5ef1e9d23d72e944c7cf2540b5e53a7b718973bf
franek1519 <franek1519@gmail.com>
2025-12-24 23:00:16 +0100

    Test push_block function.

--- src/backend/test/ringbuffertest.cpp
+++ src/backend/test/ringbuffertest.cpp
@@ -18,26 +18,31 @@ protected:
 
 TEST_F(RingBufferTests, push_block)
 {
-    RingBuffer<float, 64> ringBuffer{};
+    RingBuffer<float, 8> ringBuffer{};
 
     float tab[8]{1,2,3,4,5,6,7,8};
 
-    ringBuffer.push_block(tab, 8);
+    float readTab[2]{};
 
-
-    float* readTab = new float[4];
-
-    ringBuffer.print_buffer();
-
-    ringBuffer.pop(readTab, 4);
-
+    float expectedValue = 1;
+    // check if the whole tab pushed and popped from the buffer correctly
     for (uint16_t x = 0; x < 4; ++x)
     {
-        std::cout << readTab[x] << ' ';
-    }
-    std::cout << std::endl;
+        ringBuffer.push_block(tab + (x * 2), 2);
+        ringBuffer.pop(readTab, 2);
 
-    delete[] readTab;
+        EXPECT_EQ(readTab[0], expectedValue);
+        EXPECT_EQ(readTab[1], expectedValue + 1.f);
+        expectedValue += 2.f;
+    }
+
+    // check if the values are pushed and popped correctly after exceeding the ring buffers capacity
+    expectedValue = 1.f;
+    ringBuffer.push_block(tab, 2);
+    ringBuffer.pop(readTab, 2);
+
+    EXPECT_EQ(readTab[0], expectedValue);
+    EXPECT_EQ(readTab[1], expectedValue + 1.f);
 }
 
 TEST_F(RingBufferTests, MultiThreadedPush)
@@ -45,7 +50,8 @@ TEST_F(RingBufferTests, MultiThreadedPush)
     RingBuffer<float, 64> ringBuffer{};
 
     auto producerWork = [&]() {
-        for(int i = 0; i < 100; i++)
+        for(int i = 0; i < 3
+            ; i++)
         {
 
             float data[8];
@@ -55,9 +61,29 @@ TEST_F(RingBufferTests, MultiThreadedPush)
             }
 
             ringBuffer.push_block(data, 8);
-            std::this_thread::sleep_for(std::chrono::milliseconds(50));
         }  
     };
 
-    EXPECT_EQ(1, 1);
+    float readBuffer[24]{0.0f};
+
+    auto consoomerWork = [&]() {
+        for (int i = 0; i < 3; i++)
+        {
+            ringBuffer.pop(readBuffer + (i * 8), 8);
+        }
+    };
+
+    std::thread producerThread(producerWork);
+    std::thread consoomerThread(consoomerWork);
+
+    
+    producerThread.join();
+    consoomerThread.join();
+
+    for (int idx = 0; idx < 24; idx++)
+    {
+        std::cout << readBuffer[idx] << std::endl;
+    }
+
+    EXPECT_EQ(readBuffer[10], 100.f);
 }
