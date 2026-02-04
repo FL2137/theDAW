8b6f12a3ca0d4e701cdf59af2caa907d54188c26
franek1519 <franek1519@gmail.com>
2026-02-04 18:44:02 +0100

    Vulkan stuff

--- src/gui/waveview.hpp
+++ src/gui/waveview.hpp
@@ -2,19 +2,52 @@
 #define WAVEVIEW_HPP
 
 #include <QOpenGLWidget>
+#include <QOpenGLFunctions>
+#include <QVulkanWindow>
+#include <QVulkanFunctions>
+#include <QVulkanExtension>
+#include <QVulkanLayer>
+#include <gl/GL.h>
 
-
-class WaveView : public QOpenGLWidget
+class WaveVulkanRenderer : public QVulkanWindowRenderer
 {
+
+public:
+    WaveVulkanRenderer(QVulkanWindow* vulkanWindow) : m_vulkanWindow(vulkanWindow)
+    {
+        m_vulkanFunctions = nullptr;
+
+        m_vulkanInstance.create();
+
+        m_vulkanWindow->setVulkanInstance(&m_vulkanInstance);
+    }
+
+    void startNextFrame() override {
+        VkCommandBuffer cmdBuffer = m_vulkanWindow->currentCommandBuffer();
+
+        // m_vulkanFunctions->vkCmdBeginRenderPass(cmdBuffer, VkRenderPass
+
+        m_vulkanWindow->frameReady();
+    }
 
 
 protected:
-    void initializeGL() override
-    {
-        QOpenGLContext* openglCtx = QOpenGLContext::currentContext()->functions();
-        
-    }
-}
 
+private:
+    QVulkanWindow* m_vulkanWindow;
+    QVulkanDeviceFunctions* m_vulkanFunctions;
+    QVulkanInstance m_vulkanInstance;
+};
+
+class VulkanWidget : public QVulkanWindow {
+public:
+
+    QVulkanWindowRenderer* createRenderer() override {
+        return new WaveVulkanRenderer(this);
+    }
+
+private:
+
+};
 
 #endif // WAVEVIEW_HPP
